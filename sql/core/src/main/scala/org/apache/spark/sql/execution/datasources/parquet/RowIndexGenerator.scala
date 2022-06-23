ac933a2b2518225f9875be361ea3db279e23f1a8
Ala Luszczak <ala@databricks.com>
2022-06-23 17:04:49 +0200

    4 failing tests

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/parquet/RowIndexGenerator.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/parquet/RowIndexGenerator.scala
@@ -24,10 +24,6 @@ import org.apache.spark.sql.execution.vectorized.WritableColumnVector
 import org.apache.spark.sql.types.StructField
 import org.apache.spark.sql.types.StructType
 
-// import org.apache.hadoop.mapreduce.RecordReader
-// import org.apache.parquet.hadoop.ParquetRecordReader
-// import org.apache.spark.sql.catalyst.InternalRow
-
 /**
  * Generate row index across batches for a file.
  */
